8fb99ebd696b346bf2675c87c20e3f2e02566c2e
Jefferson Abraham Dermawan <jeffersonabrahamdermawan1217@gmail.com>
2026-06-11 16:54:12 +0700

    Complete homepage frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Card from './pages/Card/Card'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
-    <div className="app">
-        <NavBar />
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/cart" element={<Card />} />
-            <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
-    </div>
+    <>
+        <div className="app">
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/cart" element={<Card />} />
+                <Route path="/order" element={<PlaceOrder />} />
+            </Routes>
+        </div>
+        <Footer />
+    </>
   )
 }
 
